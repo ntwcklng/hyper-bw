75249e79567c4f0f5cdf5bd4bfd6b666427806a5
ntwcklng <ntwcklng@gmail.com>
2017-09-28 10:28:24 +0200

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+const WHITE = '#ffffff'
+const BLACK = '#000000'
+const backgroundColor = BLACK
+const foregroundColor = WHITE
+const borderColor = backgroundColor;
+
+const colors = {
+  black: BLACK,
+  red: WHITE,
+  green: WHITE,
+  yellow: WHITE,
+  blue: WHITE,
+  magenta: WHITE,
+  cyan: WHITE,
+  white: WHITE,
+  lightBlack: WHITE,
+  lightRed: WHITE,
+  lightGreen: WHITE,
+  lightYellow: WHITE,
+  lightBlue: WHITE,
+  lightMagenta: WHITE,
+  lightCyan: WHITE,
+  colorCubes: WHITE,
+  grayscale: foregroundColor
+};
+
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+    foregroundColor,
+    backgroundColor,
+    borderColor,
+    cursorColor,
+    colors,
+    css: `
+      ${config.css || ''}
+      .cursor-node {
+              backgroundColor: ${WHITE} !important;
+              border-color: ${WHITE} !important;
+      }
+      .hyper_main {
+        border: none !important;
+      }
+      .tab_tab {
+        border: 0;
+      }
+      .tab_textActive {
+        border-bottom: 2px solid ${WHITE};
+      }
+    `
+  });
+};
